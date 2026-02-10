f07b856ad35964f74d5eb0d923ebd3dd8c0b1d1e
Apirak Sang-ngenchai <apiruk326@gmail.com>
2026-02-10 01:18:54 +0000

    now the name of repo is studio on my github, what should i do

---
+++ src/next.config.ts
@@ -0,0 +1,39 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  // This should be the name of your GitHub repository.
+  basePath: '/studio',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // Image optimization is not available for static exports.
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
